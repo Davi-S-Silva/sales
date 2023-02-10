23754096384034ece7ccb8a3162e3e47b263036e
davi-s-silva <daviedeus43@gmail.com>
2023-02-10 16:54:35 -0300

    update system
    inserindo manual
    inserindo psd do layout
    atualizando readme.md
    rota do manual inserida

--- README.md
+++ README.md
@@ -2,9 +2,20 @@
 ##  Sistema de controle da Sales transporte
 
  <ul>
-    <li>AS</li>
+    <li>AS
+      <ul>   
+         <li> Inserir -> <i>Inserir nova AS</i></li>
+         <li> Consultar</li>
+         <li> </li>
+      </ul>
+    </li>
     <li>Notas</li>    
     <li>Veiculos</li>    
-    <li>Funcionarios</li>    
+    <li>Funcionarios
+      <ul>
+         <li>Mot</li>
+         <li>Ajud</li>
+      </ul>
+    </li>    
     <li>...</li>    
  </ul>
---
+++ resources/views/manual.blade.php
@@ -0,0 +1,101 @@
+<ul>
+    <li>
+        <h1>Login</h1>
+        <p>Nesta tela fazer o login no sistema com os dados fornecidos pelo adm do sistema.</p>
+        <p>login: primeiro e segundo nome do usuario; senha: os 6 primeiros numero do cpf do usuario;</p>
+    </li>
+    <li>
+        <h1>Dashboard</h1>
+        <p>Nesta tela mostra as informaçoes mais usadas do sistema. Veiculos fazendo entregas com suas respectivas AS e notas</p>
+        <p>Nesta tela mostra as quantidades de AS totais do sistema e as abertas e fechadas. o mesmo para as notas de cada AS.
+        </p>
+    </li>
+
+    <li>
+        <h1>Cadastro necessarios</h1>
+        <p> Nesta Tela se cadastra tudo que vai ser usado no sistema para fazer as entregas das AS</p>
+        <ul>
+            <li>
+                <h1>Veiculos</h1>
+                <p>Nesta tela Cadastra-se ou Consulta-se os Veiculos. placa, data vencimento ipva...</p>
+            </li>
+            <li>
+                <h1>Colaboradores</h1>
+                <p>Nesta tela Cadastra-se ou Consulta-se os colaboradores. casa ou chapa; motorista/ajudante</p>
+            </li>
+        </ul>
+    </li>
+
+    <li>
+        <h1>AS</h1>
+        <p>Nesta tela Cadastra-se ou Consulta-se as AS</p>
+        <p>Inserir o numero da AS, o caminhao, motorista e ajudante. se for passar a carga pra outro caminhao inserir aqui as informacoes. <br> 
+            inserir uma foto das notas que compem a AS localizada em notas fiscais relacionadas.</p>
+    </li>
+    <li>
+        <h1>Notas</h1>
+        <p>Nesta tela Cadastra, Consulta ou Encerra as notas das AS</p>
+    </li>
+    <li>
+        <h1>Relatórios</h1>
+        <p>Nesta tela obtem-se todos os relatórios do sistema</p>
+        <p>
+            <li>retorno por motorista - todas as notas retornadas pelo motorista</li>
+            <li>AS por motorista - as entregas que esse motorista fez</li>
+            <li>AS por Veiculos - as entregas que esse veiculo fez</li>
+            <li>usuarios - adm, gestor, mot, ajudante...</li>
+            <li>notas - todas as notas do sistema</li>
+            <li>Filial - recife, caruaru...</li>
+        </p>
+    </li>
+
+    <li>
+        <h1>Acesso Motorista</h1>
+        <p>mostra a AS que esta ativa na sua entrega juntamente com as respectivas notas. cada nota tem acesso a poder da baixa inserindo corretamente as informacoes necessarias.</p>
+        <ul>
+            <h2>informacoes necessarias</h2>
+            <li>é retorno? sim: escrever a observacao sobre o motivo do retorno. nao: pode passar pra proxima informacao.</li>
+            <li>qual forma de pagamento? boleto, a vista, bonificacao - marcar a opcao correspondente</li>
+            <li>valor? se for bonificacao deixar em branco.</li>
+            <li>comprovante? so aparece se nao for retorno. - foto do comprovante da maquina ou pix.</li>
+        </ul>
+        <p>obs* o motorista deve entrar em contato com o vendedor e com o seu superior responsavel.</p>
+    </li>
+</ul>
+
+
+
+<ul> <h1>Menu Principal</h1>
+    <li>AS <i>item do menu das AS do sistema</i>
+      <ul>   
+         <li> Inserir - <i>Inserir nova AS</i></li>
+         <li> Consultar - <i> Consultar AS inserida</i></li>
+      </ul>
+    </li>
+    <li>Notas <i>item do menu das notas do sistema</i>
+        <ul>
+            <li>
+                Notas - <i>Todas as notas</i>
+            </li>
+            <li>
+                Notas Abertas - <i>Todas as notas que estao em aberto</i>
+            </li>
+            <li>
+                Notas Concluidas - <i>Todas as notas que estao concluidas a entrega</i>
+            </li>
+        </ul>
+    </li>    
+    <li>Veiculos</li>    
+    <li>Funcionarios
+      <ul>
+         <li>Motorista</li>
+         <li>Ajuddante</li>
+      </ul>
+    </li>    
+    <li>...</li>    
+ </ul>
+
+
+
+
+ 
